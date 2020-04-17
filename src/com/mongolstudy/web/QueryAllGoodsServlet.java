7e27bf1ad234ec33d7b2f9ea54c9bd7f5063c523
20162103956 <1969912182@qq.com>
2020-04-17 21:58:53 +0800

    echarts可视化

--- src/com/mongolstudy/web/QueryAllGoodsServlet.java
+++ src/com/mongolstudy/web/QueryAllGoodsServlet.java
@@ -1,5 +1,6 @@
 package com.mongolstudy.web;
 
+import com.alibaba.fastjson.JSON;
 import com.mongolstudy.bean.Goods;
 import com.mongolstudy.dao.GoodsDao;
 import com.mongolstudy.service.GoodsService;
@@ -10,20 +11,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.List;
 
 @WebServlet(name = "queryAllGoodsServlet", value = "/queryAllGoodsServlet")
 public class QueryAllGoodsServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getSession().getAttribute("uid");
-        GoodsService goodsService = new GoodsService();
-        String goodsJson = GoodsService.queryAll();
+        List<Goods> goodsList;
+        GoodsDao goodsDao = new GoodsDao();
+        try {
+            goodsList = goodsDao.queryAllGoods();
+            request.setAttribute("goodsList", goodsList);
+            String s = JSON.toJSONString(goodsList);
+            response.getWriter().print(s);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         //3、响应数据:
-        response.getWriter().print(goodsJson);
+
 
 
     }
